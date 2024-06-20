ae18974985353ccb41d1cfc06ebf499c10ae57d2
iainiRX <3426767388@qq.com>
2024-06-20 19:35:55 +0800

    Site updated: 2024-06-20 19:35:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/11/25/心路历程/","2024/06/20/测试/"];function toRandomPost(){
+var posts=["2022/11/25/心路历程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
