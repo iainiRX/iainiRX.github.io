ed5997f2ffcdbd33d36920ae018f15814148f903
iainiRX <3426767388@qq.com>
2024-06-21 09:44:16 +0800

    Site updated: 2024-06-21 09:44:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/11/25/心路历程/"];function toRandomPost(){
+var posts=["2024/06/21/OCS网课助手/","2022/11/25/心路历程/","2024/06/21/标签语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
