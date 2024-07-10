d726429f3e1ad4cdbbae2428a9f8215f415c992d
iainiRX <3426767388@qq.com>
2024-07-10 21:22:23 +0800

    Site updated: 2024-07-10 21:22:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/21/OCS网课助手/","2022/11/25/心路历程/","2024/06/21/标签语法/","2024/06/27/打工人操作题/"];function toRandomPost(){
+var posts=["2024/06/21/OCS网课助手/","2022/11/25/心路历程/","2024/06/27/打工人操作题/","2024/06/21/标签语法/","2024/07/10/艾宾浩斯遗忘曲线/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
