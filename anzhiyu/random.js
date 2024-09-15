03028123d7da172dcf0ccd6897baebd421c328b3
iainiRX <3426767388@qq.com>
2024-09-15 22:04:24 +0800

    Site updated: 2024-09-15 22:04:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,56 @@
-var posts=["2024/06/21/OCS网课助手/","2024/09/09/hyper-v安装centos7-6/","2024/09/05/linux系统-centos/","2022/11/25/心路历程/","2024/06/27/打工人操作题/","2024/06/21/标签语法/","2024/09/10/无界3-0新手教程/","2024/07/10/艾宾浩斯遗忘曲线/","2024/08/28/青龙面板教程-QQRobot/","2024/09/12/从0到1搭建青龙面板/"];function toRandomPost(){
+var posts=["2024/06/21/OCS网课助手/","2024/09/09/hyper-v安装centos7-6/","2024/09/05/linux系统-centos/","2024/09/12/从0到1搭建青龙面板/","2022/11/25/心路历程/","2024/09/10/无界3-0新手教程/","2024/06/21/标签语法/","2024/06/27/打工人操作题/","2024/07/10/艾宾浩斯遗忘曲线/","2024/08/28/青龙面板教程-QQRobot/","2024/09/13/宝塔破解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"Ash","link":"https://www.ashblog.top","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A2161229155190231137135-20240514133138.svg","descr":"Future is now."},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"},{"name":"神奇海螺","link":"https://rxgo.cn","avatar":"https://iainixzy-image-1304973859.cos.ap-shanghai.myqcloud.com/iainixzy-image-1304973859/%E5%8D%9A%E5%AE%A22e077e4212f0c8449769f16a12b3516.svg","descr":"凡事预则立, 不预则废"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
